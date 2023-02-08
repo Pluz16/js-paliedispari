03c088338df4d8ef2b946006ce1285b174d29487
Francesco Punta <puntafrancesco@gmail.com>
2023-02-08 16:22:45 +0100

    Palindroma corretto

--- js/app.js
+++ js/app.js
@@ -3,18 +3,19 @@
 let parola = (prompt('Inserire una parola palindroma'))
 
 // Creare una funzione per capire se la parola inserita è palindroma
-function palindromo(str) {
-    const parola = str.toLowerCase().replace(regex, '');
-	const lunghezza = parola.length;
+function palindroma() {
+    var risultato = parola.split("").reverse().join("");
+    
+       if (parola == risultato) {
+         return parola + " è palindroma";
+       }
+    
+       else {
+         return parola + " non è palindroma";
+       }
+     }
 
-	for (let i = 0; i < lunghezza / 2; i++) {
-		if (parola[i] !== parola[lunghezza - 1 - i]) {
-			return false;
-		}
-	}
-
-	return true;
-}
-console.log(palindromo('tomomot'));
+     var controllo = palindroma(parola);
+        console.log(controllo);
 
 // Comunicare all’utente (decidete voi come) se la parola che ha inserito è palindroma oppure no
