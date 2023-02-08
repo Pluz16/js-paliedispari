e9322103d4f2550a5bd1efc8fee142255814b02a
Francesco Punta <puntafrancesco@gmail.com>
2023-02-08 17:00:32 +0100

    Fine pari dispari

--- js/app.js
+++ js/app.js
@@ -19,3 +19,39 @@ function palindroma() {
         console.log(controllo);
 
 // Comunicare all’utente (decidete voi come) se la parola che ha inserito è palindroma oppure no
+
+
+
+// Pari o dispari
+
+// 2__Gioco Pari e dispari:
+// L’utente sceglie pari o dispari e un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1
+// a 5) per il computer.
+// Sommiamo i due numeri e dichiariamo chi ha
+// vinto (controlliamo se la somma da pari o dispari).
+
+ var scelta1 = prompt("Scrivi pari o dispari");
+ var sceltaGiocatore = parseInt(prompt("scrivi un numero da 1 a 5"));
+ var sceltaComputer = getRandomIntInclusive(1,5);
+ var numero = console.log(sceltaComputer);
+ var sommaNumeri = sceltaGiocatore + sceltaComputer;
+
+ function getRandomIntInclusive(min, max) {
+   min = Math.ceil(min);
+   max = Math.floor(max);
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+ }
+
+ if (sommaNumeri % 2 == 0) {
+   console.log(sommaNumeri + " è pari");
+ }
+ else {
+   console.log(sommaNumeri + " è dispari");
+ }
+
+ if(sceltaComputer < sceltaGiocatore){
+    console.log('vince utente')
+ } else{
+    console.log('vince computer')
+ }
