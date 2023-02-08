c680f3ab3dff979f882bcdea1f6a4a6626521ef4
Francesco Punta <puntafrancesco@gmail.com>
2023-02-08 17:33:09 +0100

    Es parlante

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ let parola = (prompt('Inserire una parola palindroma'))
 
 // Creare una funzione per capire se la parola inserita è palindroma
 function palindroma() {
-    var risultato = parola.split("").reverse().join("");
+    var risultatoFx = parola.split("").reverse().join("");
     
-       if (parola == risultato) {
+       if (parola == risultatoFx) {
          return parola + " è palindroma";
        }
     
@@ -24,25 +24,22 @@ function palindroma() {
 
 // Pari o dispari
 
-// 2__Gioco Pari e dispari:
 // L’utente sceglie pari o dispari e un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1
-// a 5) per il computer.
-// Sommiamo i due numeri e dichiariamo chi ha
-// vinto (controlliamo se la somma da pari o dispari).
-
  var scelta1 = prompt("Scrivi pari o dispari");
  var sceltaGiocatore = parseInt(prompt("scrivi un numero da 1 a 5"));
  var sceltaComputer = getRandomIntInclusive(1,5);
  var numero = console.log(sceltaComputer);
+//  Sommiamo i due numeri
  var sommaNumeri = sceltaGiocatore + sceltaComputer;
 
+ // Generiamo un numero random (sempre da 1 a 5) per il computer.
  function getRandomIntInclusive(min, max) {
    min = Math.ceil(min);
    max = Math.floor(max);
    return Math.floor(Math.random() * (max - min + 1)) + min;
  }
 
+//  (controlliamo se la somma da pari o dispari)
  if (sommaNumeri % 2 == 0) {
    console.log(sommaNumeri + " è pari");
  }
@@ -50,6 +47,8 @@ function palindroma() {
    console.log(sommaNumeri + " è dispari");
  }
 
+
+//  dichiariamo chi ha vinto .
  if(sceltaComputer < sceltaGiocatore){
     console.log('vince utente')
  } else{
